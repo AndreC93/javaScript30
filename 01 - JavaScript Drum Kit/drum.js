8b7e82b7d9eb445d9f8928f49709de8bb20b34ae
Andre <fendred@yahoo.com>
2019-01-02 16:45:11 -0500

    Implement drum set functionality

---
+++ 01 - JavaScript Drum Kit/drum.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('.key').forEach( key => {
+  key.addEventListener('transitionend', (e) => {
+      if(e.propertyName === 'transform') {
+        key.classList.remove('playing');
+      }
+  });
+});
+
+window.addEventListener('keydown', (e) => {
+  const key = document.querySelector(`div[data-key='${e.keyCode}']`);
+  if(!key) return;
+  key.classList.add('playing');
+  playSound(e.keyCode);
+});
+
+const playSound = keyCode => {
+  const audio = document.querySelector(`audio[data-key='${keyCode}']`);
+  if(!audio) return;
+  audio.currentTime = 0;
+  audio.play();
+};
