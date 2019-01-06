eba4ae15fb394c76f2a7e66ef506eaadc655cfb9
Andre <fendred@yahoo.com>
2019-01-06 14:57:29 -0500

    Implement CSS Variables changing to inputs

--- 03 - CSS Variables/cssVariables.js
+++ 03 - CSS Variables/cssVariables.js
@@ -1,7 +1,17 @@
 class CssVariables {
   constructor() {
-    this.img = document.querySelector('img');
+    this.inputs = document.querySelectorAll('.controls input');
+    this.inputs.forEach(input => input.addEventListener('change', this.handleUpdate));
+    this.inputs.forEach(input => input.addEventListener('mousemove', this.handleUpdate));
+  }
+
+  handleUpdate() {
+    const suffix = this.dataset.sizing || '';
+    const root = document.documentElement;
+    root.style.setProperty(`--${this.name}`, this.value + suffix);
   }
 
 
 }
+
+const cssVariables = new CssVariables();
